e82f9d19d6ed8a752c60ae2fe83ff8ac57792364
Steve Mustanski <steve.mustanski@capitalone.com>
2017-12-30 00:16:15 -0500

    working geolocation plus distance to wickedly smart hq

---
+++ src/chap5/myLoc.js
@@ -0,0 +1,65 @@
+window.onload = getMyLocation;
+
+var ourCords = {
+  latitude: 47.624851,
+  longitude: -112.52099
+}
+
+function getMyLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+  }else {
+    alert("Oops, no geolocation support detected");
+  }
+  
+}
+
+function displayLocation(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  var div = document.getElementById("location");
+  div.innerHTML = "You are at latitude: " + latitude + ", Longitude: " + longitude;
+
+  var km = computeDistance(position.coords, ourCords);
+
+  var distance = document.getElementById("distance");
+  distance.innerHTML = "You are " + km + " km from wickedly smart HQ";
+}
+
+function displayError(error) {
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied",
+		2: "Position is not available",
+		3: "Request timeout"
+	};
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2) {
+		errorMessage = errorMessage + " " + error.message;
+	}
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
+}
+
+// Uses the Spherical Law of Cosines to find the distance
+// between two lat/long points
+//
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	var Radius = 6371; // radius of the Earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) +
+					Math.cos(startLatRads) * Math.cos(destLatRads) *
+					Math.cos(startLongRads - destLongRads)) * Radius;
+
+	return distance;
+}
+
+function degreesToRadians(degrees) {
+	radians = (degrees * Math.PI)/180;
+	return radians;
+}
